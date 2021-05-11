810670dd18f045e02963ab967bc01db6a8a01ed9
Spenc100 <66834838+Spenc100@users.noreply.github.com>
2021-05-11 11:36:20 -0400

    Update Pico_motor.py

--- Pico_motor.py
+++ Pico_motor.py
@@ -17,16 +17,16 @@ min_out=100
 
 def pwm_prog():
     # fmt: off
-    pull(noblock) .side(0)
+    pull(noblock).side(0)
     mov(x, osr) 
     mov(y, isr) 
     
     #start pwm loop
-    jmp(x_not_y, "skip")
+    jmp(x_not_y,"skip")
     label("pwmloop")
-    nop()         .side(1)
+    nop().side(1)
     label("skip")
-    jmp(y_dec, "pwmloop")
+    jmp(y_dec,"pwmloop")
     # fmt: on
 
 class PIOPWM:
@@ -40,8 +40,7 @@ class PIOPWM:
         self._max_count = max_count
 
     def set(self, value):
-#        #minimum value is -1 (which turns off motor), 0 still produce a narrow pulse
-        value = max(value, -1)
+        value = max(value,-1)
         value = min(value, self._max_count)
         self._sm.put(value)
 
@@ -49,24 +48,23 @@ class PIOPWM:
 mtr_sm= StateMachine(0, pwm_prog, freq=10000000, sideset_base=Pin(15))
 mtr_sm.put(max_counting)
 
-#Use exec() to load max count into ISR
 mtr_sm.exec("pull()")
 mtr_sm.exec("mov(isr, osr)")
 mtr_sm.active(1)
+
+#Smooth motor motion
+for t in range(max_counting-min_out):
+    mtr_sm.put(t+min_out)
+    sleep(0.001)
+sleep(2.5)
+for t in range(max_counting):
+    mtr_sm.put(max_counting-t)
+    sleep(0.001)
+mtr_sm.put(0)
 
 #Jerky motor motion
 mtr_sm.put(0)
 sleep(0.5)
 mtr_sm.put(max_counting)
 sleep(2.5)
-mtr_sm.put(0)
-
-#Smooth motor motion
-for i in range(max_counting-min_out):
-    mtr_sm.put(i+min_out)
-    sleep(0.001)
-sleep(2.5)
-for i in range(max_counting):
-    mtr_sm.put(max_counting-i)
-    sleep(0.001)
 mtr_sm.put(0)
